982e3afcfb048da04d23654d420743965d921451
redyaung <redyaung2003@gmail.com>
2024-03-13 03:27:38 -0400

    variable block and word counts

--- main.cpp
+++ main.cpp
@@ -4,20 +4,24 @@
 #include <stdexcept>
 #include <algorithm>
 #include <cassert>
+#include <initializer_list>
 
-bool isWordAligned(uint32_t addr) {
-  return addr % 4 == 0;
+constexpr uint32_t nbytes(uint32_t nwords) {
+  return nwords * 4;
 }
+
+// forward declarations
+bool isAligned(uint32_t addr, uint32_t nwords);
 
 // 32-bit word
 struct Word {
-  std::byte bytes[4];
+  std::byte bytes[nbytes(1)];
 
   Word() = default;
   Word(uint32_t value) {
     auto b = std::bit_cast<std::byte*>(&value);
     // this probably only works for little endian
-    for (auto i = 0; i < 4; i++) {
+    for (auto i = 0; i < nbytes(1); i++) {
       bytes[i] = b[i];
     }
   }
@@ -27,27 +31,61 @@ struct Word {
   }
 };
 
+// a block containing w words
+template<uint32_t W>
+struct Block {
+  std::byte bytes[nbytes(W)];
+
+  Block() = default;
+  // better way: refer to make_tuple()
+  Block(std::initializer_list<Word> words) {
+    if (words.size() != W) {
+      throw std::invalid_argument("number of words is not equal to W");
+    }
+    auto wordIdx = 0;
+    for (auto it = words.begin(); it != words.end(); it++, wordIdx++) {
+      for (auto byteIdx = 0; byteIdx < nbytes(1); byteIdx++) {
+        bytes[nbytes(wordIdx) + byteIdx] = (*it).bytes[byteIdx];
+      }
+    }
+  }
+
+  Word operator[](int wordIdx) const {
+    if (wordIdx < 0 || wordIdx >= W) {
+      throw std::out_of_range("word index is out of bounds");
+    }
+    auto wordPtr = std::bit_cast<Word*>(&bytes[nbytes(wordIdx)]);
+    return std::bit_cast<Word>(*wordPtr);
+  }
+};
+
 // main memory (N=8-bit address space = 2^8 = 256bytes)
-template<uint8_t N>
+template<uint32_t N>
 struct MainMemory {
   std::byte bytes[1<< N];
 
-  Word readWord(uint32_t addr) {
+  template<uint32_t W>
+  Block<W> readBlock(uint32_t addr) {
     std::cout << "reading from memory" << std::endl;
-    if (!isWordAligned(addr)) {
+    if (!isAligned(addr, W)) {
       throw std::invalid_argument("address is not word-aligned");
+    } else if (addr + nbytes(W) >= (1<< N)) {
+      throw std::out_of_range("end address is out of address space");
     }
-    Word word;
-    std::copy(bytes + addr, bytes + addr + 4, word.bytes);
-    return word;
+    Block<W> block;
+    std::copy(bytes + addr, bytes + addr + nbytes(W), block.bytes);
+    return block;
   }
 
-  void writeWord(uint32_t addr, Word word) {
+  template<uint32_t W>
+  void writeBlock(uint32_t addr, Block<W> block) {
     std::cout << "writing to memory" << std::endl;
-    if (!isWordAligned(addr)) {
+    if (!isAligned(addr, W)) {
       throw std::invalid_argument("address is not word-aligned");
+    } else if (addr + nbytes(W) >= (1<< N)) {
+      throw std::out_of_range("end address is out of address space");
     }
-    std::copy(word.bytes, word.bytes + 4, &bytes[addr]);
+    std::copy(block.bytes, block.bytes + nbytes(W), bytes + addr);
   }
 };
 
@@ -58,72 +96,150 @@ struct MainMemory {
 // simple write-through write-allocate cache
 // one word per block; 4 blocks in cache -- 4 * 4bytes = 16bytes
 // fields: |  tag(28)  | index(2) | block(0) | word(2) |
+
+// W: number of words in a block
+// B: number of blocks in the cache
+// N: address space of the underlying main memory
+// fields: | tag(rem) | index(log(B)) | block(log(W)) | word(2) |
+template<uint32_t W, uint32_t B, uint32_t N>
 struct Cache {
+  static_assert(std::has_single_bit(W), "W must be a power of 2");
+  static_assert(std::has_single_bit(B), "B must be a power of 2");
+  static constexpr uint32_t nBlockBits = static_cast<uint32_t>(std::countr_zero(W));
+  static constexpr uint32_t nIndexBits = static_cast<uint32_t>(std::countr_zero(B));
+
   struct Entry {
     bool valid;
     uint32_t tag;
-    Word block;
+    Block<W> block;
   };
 
-  Entry entries[4];
-  MainMemory<8> mainMem;
+  Entry entries[B];
+  MainMemory<N> mainMem;
 
-  Cache(MainMemory<8> mainMem) : mainMem{mainMem} {}
+  Cache(MainMemory<N> mainMem) : mainMem{mainMem} {}
 
-  Word readWord(uint32_t addr) {
-    if (!isWordAligned(addr)) {
+  template<uint32_t _W>
+  Block<_W> readBlock(uint32_t addr) {
+    static_assert(W % _W == 0, "requested block size must divide internal block size");
+    if (!isAligned(addr, _W)) {
       throw std::invalid_argument("address is not word-aligned");
     }
     uint32_t tag = tagBits(addr);
     uint32_t index = indexBits(addr);
     if (!entries[index].valid || entries[index].tag != tag) { // cache miss
       std::cout << "cache miss" << std::endl;
-      Word word = mainMem.readWord(addr);
-      entries[index] = Entry{true, tag, word};
+      uint32_t startingAddr = (addr / nbytes(W)) * nbytes(W);
+      Block block = mainMem.template readBlock<W>(startingAddr);
+      entries[index] = {true, tag, block};
     } else {
       std::cout << "cache hit" << std::endl;
     }
-    return entries[index].block;
+    const Block<W> &entryBlock = entries[index].block;
+    Block<_W> requestedBlock;
+    uint32_t blockAddr = addr % nbytes(W);
+    std::copy(entryBlock.bytes + blockAddr, entryBlock.bytes + blockAddr + nbytes(_W),
+      requestedBlock.bytes);
+    return requestedBlock;
   }
 
-  void writeWord(uint32_t addr, Word word) {
-    if (!isWordAligned(addr)) {
+  template<uint32_t _W>
+  void writeBlock(uint32_t addr, Block<_W> block) {
+    static_assert(W % _W == 0, "requested block size must divide internal block size");
+    if (!isAligned(addr, _W)) {
       throw std::invalid_argument("address is not word-aligned");
     }
     uint32_t tag = tagBits(addr);
     uint32_t index = indexBits(addr);
     if (entries[index].valid && entries[index].tag == tag) {  // cache hit
       std::cout << "cache hit - writing to cache entry" << std::endl;
-      entries[index].block = word;
+      Block<W> &entryBlock = entries[index].block;
+      uint32_t blockAddr = addr % nbytes(W);
+      std::copy(block.bytes, block.bytes + nbytes(_W), entryBlock.bytes + blockAddr);
     }
-    mainMem.writeWord(addr, word);
+    mainMem.template writeBlock(addr, block);
   }
 
   uint32_t tagBits(uint32_t addr) {
-    return addr>> 4;
+    return addr>> (nIndexBits + nBlockBits + 2);
   }
+
   uint32_t indexBits(uint32_t addr) {
-    return (addr>> 2) & 0b11;
+    return (addr>> (nBlockBits + 2)) & ((1<< nIndexBits) - 1);
   }
 };
 
+// Checks if address is aligned on an n-word boundary
+bool isAligned(uint32_t addr, uint32_t nwords) {
+  return addr % nbytes(nwords) == 0;
+}
+
+std::ostream& operator<<(std::ostream& os, const Word& word) {
+  os << std::hex << uint32_t(word);
+  return os;
+}
+
+template<uint32_t W>
+std::ostream& operator<<(std::ostream& os, const Block<W>& block) {
+  os << "{";
+  for (auto wordIdx = 0; wordIdx < W; wordIdx++) {
+    os << block[wordIdx];
+    if (wordIdx < W - 1) {
+      os << ", ";
+    }
+  }
+  os << "}";
+  return os;
+}
+
 int main() {
   MainMemory<8> mem;
-  Cache cache{ mem };
+  Cache<4, 4, 8> cache{ mem };
 
-  cache.writeWord(0x8, 0xDEADBEEFu);
-  cache.writeWord(0x18, 0xFACADEu);
+  cache.mainMem.writeBlock(0x0, Block<4>({0xAu, 0xBu, 0xCu, 0xDu}));
 
-  Word w = cache.readWord(0x8);
-  assert(uint32_t(w) == 0xDEADBEEFu);
-  std::cout << "0x8: " << std::hex << uint32_t(w) << std::endl;
+  Block w = cache.readBlock<1>(0x0);
+  std::cout << "w: " << w << std::endl;
+  assert(uint32_t(w[0]) == 0xAu);
 
-  w = cache.readWord(0x18);
-  assert(uint32_t(w) == 0xFACADEu);
-  std::cout << "0x18: " << std::hex << uint32_t(w) << std::endl;
+  w = cache.readBlock<1>(0x4);
+  std::cout << "w: " << w << std::endl;
+  assert(uint32_t(w[0]) == 0xBu);
 
-  cache.readWord(0x8);
-  cache.readWord(0x18);
+  w = cache.readBlock<1>(0x8);
+  std::cout << "w: " << w << std::endl;
+  assert(uint32_t(w[0]) == 0xCu);
+
+  w = cache.readBlock<1>(0xC);
+  std::cout << "w: " << w << std::endl;
+  assert(uint32_t(w[0]) == 0xDu);
+
+  // // Direct-mapped, 1 word per block, 4 words
+  // MainMemory<8> mem;
+  // Cache<1, 4, 8> cache{ mem };
+
+  // cache.writeBlock(0x8, Block<1>({0xDEADBEEFu}));
+  // cache.writeBlock(0x18, Block<1>({0xFACADEu}));
+
+  // Block w = cache.readBlock<1>(0x8);
+  // assert(uint32_t(w[0]) == 0xDEADBEEFu);
+  // std::cout << "0x8: " << w << std::endl;
+
+  // w = cache.readBlock<1>(0x18);
+  // assert(uint32_t(w[0]) == 0xFACADEu);
+  // std::cout << "0x18: " << w << std::endl;
+
+  // w = cache.readBlock<1>(0x8);
+  // std::cout << "0x8: " << w << std::endl;
+
+  // w = cache.readBlock<1>(0x18);
+  // std::cout << "0x18: " << w << std::endl;
+
+  // // MainMemory Test
+  // MainMemory<8> mem;
+  // mem.writeBlock(0x8, Block<2>({0xDEADBEEF, 0xBEEFCADE}));
+  // Block b = mem.readBlock<1>(0xC);
+  // std::cout << "read block: " << b << std::endl;
 
   return 0;
 }
