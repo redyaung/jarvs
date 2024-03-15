43c10e48ed3ee29a0e292403c745f70642e022ad
redyaung <redyaung2003@gmail.com>
2024-03-15 14:40:59 -0400

    add tests for dif: write schemes

--- tests/memory_test.cpp
+++ tests/memory_test.cpp
@@ -32,4 +32,38 @@ namespace {
     EXPECT_EQ(cache.readBlock<1>(0x30)[0], 0xCu);
   }
 
+  // use direct-mapped caches to avoid lru replacement issues
+  TEST(WriteThroughCacheTest, AlwaysWritesThrough) {
+    Cache<1, 1, 4, 8> cache { MainMemory<8> {} };
+    cache.writeBlock(0x4, Block<1>({0xFACADEu}));   // 0x4 not in cache
+    EXPECT_EQ(cache.mainMem.readBlock<1>(0x4)[0], 0xFACADEu);
+    cache.readBlock<1>(0x4)[0];   // now, 0x4 is in cache
+    cache.writeBlock(0x4, Block<1>({0xBEEFu}));     // still writes through
+    EXPECT_EQ(cache.mainMem.readBlock<1>(0x4)[0], 0xBEEFu);
+  }
+
+  TEST(WriteBackCacheTest, NoWriteUnlessEviction) {
+    Cache<1, 1, 4, 8, WriteScheme::WriteBack> cache { MainMemory<8> {} };
+    cache.writeBlock(0x4, Block<1>({0xFACADEu})); // 0x4 not in cache
+    EXPECT_EQ(cache.mainMem.readBlock<1>(0x4)[0], 0x0u);  // no eviction, no write
+    EXPECT_EQ(cache.readBlock<1>(0x4)[0], 0xFACADEu);
+    cache.writeBlock(0x4, Block<1>({0xBEEFu}));   // cache hit, no write
+    EXPECT_EQ(cache.mainMem.readBlock<1>(0x4)[0], 0x0u);
+    EXPECT_EQ(cache.readBlock<1>(0x4)[0], 0xBEEFu);
+  }
+
+  TEST(WriteBackCacheTest, WriteOnlyOnEviction) {
+    // field: | tag(28) | index(2) | word(2) | 
+    // 0x4 = |01|00, 0x14 = 1|01|00 -- causes conflict
+    Cache<1, 1, 4, 8, WriteScheme::WriteBack> cache { MainMemory<8> {} };
+    cache.writeBlock(0x4, Block<1>({0xFACADEu})); // 0x4 not in cache
+    EXPECT_EQ(cache.mainMem.readBlock<1>(0x4)[0], 0x0u);  // no eviction, no write
+    EXPECT_EQ(cache.readBlock<1>(0x4)[0], 0xFACADEu);
+    cache.writeBlock(0x14, Block<1>({0xBEEFu}));   // eviction, write
+    EXPECT_EQ(cache.mainMem.readBlock<1>(0x4)[0], 0xFACADEu);
+    EXPECT_EQ(cache.readBlock<1>(0x14)[0], 0xBEEFu);
+  }
+
+  // todo: add some mechanism to get cache hit/miss info
+
 }
